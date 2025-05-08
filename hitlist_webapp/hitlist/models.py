b9f3a9ae0b5b49be0709c91ccdac7442407589fd
Jozua Patalan <jozuapatalan@gmail.com>
2025-05-07 20:38:04 -0400

    connected database and added styles

--- hitlist_webapp/hitlist/models.py
+++ hitlist_webapp/hitlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    first_name = models.TextField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    status = models.TextField(max_length=20)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- hitlist_webapp/hitlist/views.py
+++ hitlist_webapp/hitlist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from . models import Note
 
 # Create your views here.
 def home(request):
-    return render(request, 'hitlist/index.html')
+    context = {
+        'Notes': Note.objects.all()
+    }
+    return render(request, 'hitlist/index.html', context)
