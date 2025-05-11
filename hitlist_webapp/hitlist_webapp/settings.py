0c622804facb4da49fd884cdde1ff2cc647f9918
Jozua Patalan <jozuapatalan@gmail.com>
2025-05-10 20:58:51 -0400

    added a partly functional sign up system
    sign up is only for email, password and confirmation of password with mid styles

--- hitlist_webapp/hitlist_webapp/settings.py
+++ hitlist_webapp/hitlist_webapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hitlist.apps.HitlistConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
