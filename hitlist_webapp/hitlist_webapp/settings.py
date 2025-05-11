8af31d8b3bf4b3ea42fbe3873e21f7114c043a99
Jozua Patalan <jozuapatalan@gmail.com>
2025-05-10 22:14:12 -0400

    added a fully functional register form
    styles not yet final

--- hitlist_webapp/hitlist_webapp/settings.py
+++ hitlist_webapp/hitlist_webapp/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hitlist.apps.HitlistConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- hitlist_webapp/users/views.py
+++ hitlist_webapp/users/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from . forms import UserRegistrationForm
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome to your hitlist, {username}!')
+            name = form.cleaned_data.get('first_name')
+            messages.success(request, f'Welcome to your hitlist, {name}!')
             return redirect('hitlist-home')
     else:
-        form = UserCreationForm() 
+        form = UserRegistrationForm() 
     return render(request, 'users/register.html', {'form': form})
