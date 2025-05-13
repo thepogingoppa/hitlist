266cd355a996896405539dcab7972dd2b01fe049
Jozua Patalan <jozuapatalan@gmail.com>
2025-05-13 16:19:04 -0400

    added a fully functional login/logout system

--- hitlist_webapp/hitlist_webapp/settings.py
+++ hitlist_webapp/hitlist_webapp/settings.py
@@ -128,3 +128,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'hitlist-home'
--- hitlist_webapp/hitlist_webapp/urls.py
+++ hitlist_webapp/hitlist_webapp/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='hitlist-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='hitlist-logout'),
     path('register/', user_views.register, name='hitlist-register'),
     path('', include('hitlist.urls')),
 ]
