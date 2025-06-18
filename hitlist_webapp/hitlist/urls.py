3645b99dcfe751757fef89bacde191a896791185
Jozua Patalan <jozuapatalan@gmail.com>
2025-06-18 17:38:30 -0400

    deleted files

--- hitlist_webapp/hitlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/', views.home, name='hitlist-home'),
-    
-    path('', views.landing, name='hitlist-landing')
-]
