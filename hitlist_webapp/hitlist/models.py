5d05c74655ad32a4f8303682a8ca2985aff058ad
Jozua Patalan <jozuapatalan@gmail.com>
2025-06-25 18:26:12 -0400

    created homepage that displays all the data in database
    created models and the dummy data and displayed them on home.html

--- hitlist_webapp/hitlist/models.py
+++ hitlist_webapp/hitlist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class People(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    
+    LOATHED = "L"
+    IGNORED = "I"
+    FORGIVEN = "F"
+
+    STATUS_CHOICES = {
+        LOATHED: "Loathed",
+        IGNORED: "Ignored",
+        FORGIVEN: "Forgiven",
+    }
+
+    status_choices = models.CharField(
+        max_length=1, 
+        choices=STATUS_CHOICES, 
+        default=LOATHED
+    )
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
