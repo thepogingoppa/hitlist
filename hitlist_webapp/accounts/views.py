c4a7addbd13dc067d00d6bfb864757045a75c543
Jozua Patalan <jozuapatalan@gmail.com>
2025-06-27 20:35:07 -0400

    created register form
    created django app, added styles, routed the urls and the main navigation links

---
+++ hitlist_webapp/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, "accounts/signup.html", context)
